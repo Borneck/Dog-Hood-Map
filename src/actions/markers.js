9cacd3804811c2043b347fab390ee460f2e7fe4f
AndyBorneck <andyBorneck@gmail.com>
2019-01-20 04:26:09 +0100

    InfoWindow Eingefügt

--- src/actions/markers.js
+++ src/actions/markers.js
@@ -1,13 +1,11 @@
 import json from '../API/locations.json';
+import markerWindow from '../components/Markers';
 
-
-
-
-export function craetedMarkers({map}) {
+export function craetedMarkers({ map }) {
    let markers = [];
    const shoppingList = json.Einkaufen;
-   console.log(json)
-   shoppingList.forEach((list, i) => { 
+
+   shoppingList.forEach((list, i) => {
       const position = list.location;
       const title = list.title;
       const marker = new window.google.maps.Marker({
@@ -17,28 +15,39 @@ export function craetedMarkers({map}) {
          title: title,
          position: position
       });
-      function markerAnmiation() { 
+      
+      function markerAnmiation() {
 
          if (marker.getAnimation() !== null) {
-           marker.setAnimation(null);
+            marker.setAnimation(null);
          } else {
-           marker.setAnimation(window.google.maps.Animation.BOUNCE);
+            marker.setAnimation(window.google.maps.Animation.BOUNCE);
          }
-       }
-      marker.addListener('click',markerAnmiation)
+      }
+     
+      const infowindow = new window.google.maps.InfoWindow({
+         content: contentString
+         
+         });
+
+      marker.addListener('click', function() {
+         markerAnmiation();
+         infowindow.open(map, marker);
+       });
+
       markers.push(marker);
-      
+
       return markers;
    })
-  
+}
 
-    
-      
-  
-   
- }
+var contentString = 'Das ist der Inhalt von InfoWindo TODO: REACT COMPONENTE ALS INHALT EINFÜGEN'
+
+
+
+
 
 export function shoppingMarkers() {
-   return {type: "Set_Shopping_Marker_Position", location: json.Einkaufen}
- }
+   return { type: "Set_Shopping_Marker_Position", location: json.Einkaufen }
+}
 
